c5c238df4028cf8e5f91eb98feff6f81bb4871a6
shimataro <shimataro@zelkova.cc>
2018-12-31 14:58:30 +0900

    add ambient declaration file for TypeScript

---
+++ index.d.ts
@@ -0,0 +1,67 @@
+declare module 'i18next-express-middleware' {
+  import express from 'express';
+  import i18next from 'i18next';
+
+  type I18next = i18next.i18n;
+  type IgnoreRoutesFunction = (req: express.Request, res: express.Response, options: HandleOptions, i18next: I18next) => boolean;
+  type Lng = string;
+  type Lngs = string[];
+  type App = express.Application | express.Router;
+
+  interface HandleOptions {
+    ignoreRoutes?: string[] | IgnoreRoutesFunction;
+    removeLngFromUrl?: boolean;
+  }
+  interface GetResourcesHandlerOptions {
+    maxAge?: number;
+    cache?: boolean;
+    lngParam?: string;
+    nsParam?: string;
+  }
+  interface MissingKeyHandlerOptions {
+    lngParam?: string;
+    nsParam?: string;
+  }
+
+  export function handle(i18next: I18next, options?: HandleOptions): express.Handler;
+  export function getResourcesHandler(i18next: I18next, options?: GetResourcesHandlerOptions): express.Handler;
+  export function missingKeyHandler(i18next: I18next, options?: MissingKeyHandlerOptions): express.Handler;
+  export function addRoute(i18next: I18next, route: string, lngs: Lngs, app: App, verb: string, fc: express.RequestHandler): void;
+  export function addRoute(i18next: I18next, route: string, lngs: Lngs, app: App, fc: express.RequestHandler): void;
+
+  // LanguageDetector
+  type LanguageDetectorServices = any;
+  type LanguageDetectorOrder = string[];
+  type LanguageDetectorCaches = boolean | string[];
+  interface LanguageDetectorOptions {
+    order?: LanguageDetectorOrder;
+    lookupQuerystring?: string;
+    lookupCookie?: string;
+    lookupSession?: string;
+    lookupFromPathIndex?: number;
+    caches?: LanguageDetectorCaches;
+    cookieExpirationDate?: Date;
+    cookieDomain?: string;
+  }
+  interface LanguageDetectorAllOptions {
+    fallbackLng: boolean | string | string[];
+  }
+  interface LanguageDetectorInterfaceOptions {
+    [name: string]: any;
+  }
+  interface LanguageDetectorInterface {
+    name: string;
+    lookup: (req: express.Request, res: express.Response, options?: LanguageDetectorInterfaceOptions) => string | string[];
+
+    cacheUserLanguage?: (req: express.Request, res: express.Response, lng: string, options?: object) => void;
+  }
+
+  export class LanguageDetector {
+    constructor(services: LanguageDetectorServices, options?: LanguageDetectorOptions, allOptions?: LanguageDetectorAllOptions);
+
+    init(services: LanguageDetectorServices, options?: LanguageDetectorOptions, allOptions?: LanguageDetectorAllOptions): void;
+    addDetector(detector: LanguageDetectorInterface): void;
+    detect(req: express.Request, res: express.Response, detectionOrder: LanguageDetectorOrder): void;
+    cacheUserLanguage(req: express.Request, res: express.Response, lng: string, caches: LanguageDetectorCaches): void;
+  }
+}
